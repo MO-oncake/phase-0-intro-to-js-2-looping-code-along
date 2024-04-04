21242a2fed353b927afe5094cd40b95677b3d1fe
William Gitau <dontysh@gmail.com>
2024-04-04 22:38:05 +0300

    creates a countdown function and a message loop log function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = [`scooby`, `daphne`, `shaggy`, `scrappy`, `teddy`];
+function writeCards(names, event) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(num) {
+    let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--);
+  }
+}
